26766825a9ee03c6248d5b70d126b251ad96d66a
esot321c <esot321c@gmail.com>
2023-07-27 19:23:56 -0700

    update meshsdk types

--- types/meshsdk.d.ts
+++ types/meshsdk.d.ts
@@ -1,5 +1,4 @@
 declare module '@meshsdk/react' {
-  export * from '@meshsdk/react/dist/components';
-  export * from '@meshsdk/react/dist/contexts';
-  export * from '@meshsdk/react/dist/hooks';
+  const content: any;
+  export = content;
 }
