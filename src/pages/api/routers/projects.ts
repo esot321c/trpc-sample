2c2ad4084c8ac0c143163a2413cde602112a04ce
esot321c <esot321c@gmail.com>
2023-07-27 19:11:38 -0700

    fix build

--- src/pages/api/routers/projects.ts
+++ src/pages/api/routers/projects.ts
@@ -1,19 +1,24 @@
 import { IProject } from '@pages/projects/[project_id]';
-import { initTRPC } from '@trpc/server';
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
 import axios from 'axios';
 
+// Initialize a context for the server
+function createContext() {
+  return {};
+}
+
+// Get the context type
+type Context = inferAsyncReturnType<typeof createContext>;
+
 // Initialize tRPC
-const t = initTRPC.create();
+const t = initTRPC.context<Context>().create();
 
 // Create a router
 export const projectsRouter = t.router({
-  queries: {
-    list: {
-      resolve: async () => {
-        const res = await axios.get(`${process.env.API_URL}/projects/`);
-        const projects = res.data.filter((project: IProject) => project.name.toLowerCase().startsWith('cardano-'));
-        return projects;
-      },
-    },
-  },
+  // List projects procedure
+  list: t.procedure.query(async () => {
+    const res = await axios.get(`${process.env.API_URL}/projects/`);
+    const projects = res.data.filter((project: IProject) => project.name.toLowerCase().startsWith('cardano-'));
+    return projects;
+  }),
 });
